bbeb3c18b3f216661c9b456866d677d217ec8833
fredericvilcot <fredericvilcot@gmail.com>
2020-09-18 18:49:58 +0200

    Updates

---
+++ precache-manifest.cc246702aa4f305937c9e6efcddd0d59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "335400e66a919f46ef0b9853177d1658",
+    "url": "/react-test-app/index.html"
+  },
+  {
+    "revision": "74343352bb4cb402b68c",
+    "url": "/react-test-app/static/css/main.27bbf825.chunk.css"
+  },
+  {
+    "revision": "7dfdd1abdb2013076cff",
+    "url": "/react-test-app/static/js/2.49cc747d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-test-app/static/js/2.49cc747d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74343352bb4cb402b68c",
+    "url": "/react-test-app/static/js/main.36a36795.chunk.js"
+  },
+  {
+    "revision": "3d3d438dc5b351084f49",
+    "url": "/react-test-app/static/js/runtime-main.6ffb19da.js"
+  }
+]);
